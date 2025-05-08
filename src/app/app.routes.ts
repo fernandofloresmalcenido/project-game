c3b3c55090d015813757dc499691895df00471ae
fernandofloresmalcenido <fernando.floresmalcenido@plexus.es>
2025-05-08 08:38:03 +0200

    style(app): update container styles with gap and background color refactor(menu): enhance MenuComponent with lifecycle hooks and event logging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { GameplayComponent } from './gameplay/gameplay.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: GameplayComponent,
+    },
+    {
+        path: 'gameplay',
+        loadComponent: () => import('./gameplay/gameplay.component').then(
+            m => m.GameplayComponent
+        ),
+    },
+];
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   @Input() title: string = 'Menu'; // Recibe el título desde el padre
 
   @Output() gameStarted = new EventEmitter<void>(); // Emite evento para "Play"
@@ -29,5 +29,45 @@ export class MenuComponent {
 
   exitGame() {
     this.gameExited.emit();
+  }
+
+  // Método del ciclo de vida: ngOnChanges
+  ngOnChanges() {
+    console.log('ngOnChanges - Cambios en @Input detectados', this.title);
+  }
+
+  // Método del ciclo de vida: ngOnInit
+  ngOnInit() {
+    console.log('ngOnInit - Componente inicializado');
+  }
+
+  // Método del ciclo de vida: ngDoCheck
+  ngDoCheck() {
+    console.log('ngDoCheck - Ciclo de detección de cambios');
+  }
+
+  // Método del ciclo de vida: ngAfterContentInit
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit - Contenido proyectado inicializado');
+  }
+
+  // Método del ciclo de vida: ngAfterContentChecked
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked - Contenido proyectado verificado');
+  }
+
+  // Método del ciclo de vida: ngAfterViewInit
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit - Vista inicializada');
+  }
+
+  // Método del ciclo de vida: ngAfterViewChecked
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked - Vista verificada');
+  }
+
+  // Método del ciclo de vida: ngOnDestroy
+  ngOnDestroy() {
+    console.log('ngOnDestroy - Componente destruido');
   }
 }
